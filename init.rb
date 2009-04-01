fa5d5b3b648087ca68c09f5daa66241085b66f27
jpr <jpr@log.pt>
2009-04-01 17:11:07 +0100

    redmine issue date shift plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_issue_date_shift do
+  name 'Redmine Issue Date Shift plugin'
+  author 'JP Ribeiro'
+  description 'Shift issue dates is a plugin for Redmine'
+  version '0.0.1'
+end
